32f9a2ffcbb95aa8f6fd2761004abad2acd49182
Avinash <magicianavi@gmail.com>
2025-09-04 11:11:31 -0400

    Just an initial commit Test

---
+++ MagicMesh/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  MagicMesh
+//
+//  Created by Avinash Sridhar on 9/4/25.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
