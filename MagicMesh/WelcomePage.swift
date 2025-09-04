7440ced7d712b4e3fed9d25981881f121d08b871
Avinash <magicianavi@gmail.com>
2025-09-04 11:47:48 -0400

    UI Changes for Welcome Page

--- MagicMesh/WelcomePage.swift
+++ MagicMesh/WelcomePage.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct WelcomePage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 300,height: 300)
+                    .foregroundStyle(.indigo)
+                
+                Image(systemName: "arrow.triangle.2.circlepath.icloud.fill")
+                    .font(.system(size:160))
+            }
+            
+            Text("Welcome to Magic Mesh")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding()
+            
+            Text("An app that integrates with all your favorite Apps using API integrations.")
+                .font(.title2)
+        }
+        .padding()
     }
 }
 
